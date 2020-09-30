81c6a7e0ac62f1f5f3f47f43a77367d4a13b0447
Steve King <steve@mydev.co>
2020-09-30 14:23:37 +0300

    Bump core version to 2.x to account for inclusion of `@kwsites/promise-result` as sample core package

---
+++ packages/promise-result/babel.config.js
@@ -0,0 +1,14 @@
+module.exports = require('@kwsites/configure-babel');
+// {
+//    presets: [
+//       [
+//          '@babel/preset-env',
+//          {
+//             targets: {
+//                node: 'current',
+//             },
+//          },
+//       ],
+//       '@babel/preset-typescript',
+//    ],
+// };
---
+++ packages/promise-result/jest.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+   preset: '@kwsites/configure-jest',
+   // roots: [
+   //    "<rootDir>/src/",
+   //    "<rootDir>/test/",
+   // ],
+   // coverageThreshold: {
+   //    global: {
+   //       branches: 80,
+   //       functions: 80,
+   //       lines: 80,
+   //       statements: 80
+   //    }
+   // },
+   // coverageReporters: ['json', 'lcov', 'text', 'clover'],
+   // testMatch: [
+   //    "**/test/**/*.spec.*"
+   // ]
+};

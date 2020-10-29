f12345aa52742f1f54b7cef44682ff77cf7d8c1c
Steve King <steve@mydev.co>
2020-10-29 09:11:15 +0000

    remove the SSH requirement

--- .github/actions/publish/entrypoint.sh
+++ .github/actions/publish/entrypoint.sh
@@ -13,10 +13,10 @@ touch ~/.npmrc
 
 # Setup SSH keys so we can push lerna commits and tags to master branch
 
-mkdir -p /root/.ssh
-ssh-keyscan -t rsa github.com > /root/.ssh/known_hosts
-echo "$GIT_DEPLOY_KEY" > /root/.ssh/id_rsa
-chmod 400 /root/.ssh/id_rsa
+#mkdir -p /root/.ssh
+#ssh-keyscan -t rsa github.com > /root/.ssh/known_hosts
+#echo "$GIT_DEPLOY_KEY" > /root/.ssh/id_rsa
+#chmod 400 /root/.ssh/id_rsa
 
 # The script is run as root so we need to allow npm to execute scripts as root.
 echo "unsafe-perm = true" >> ~/.npmrc

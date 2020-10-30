c453baee6858d077ebaf9353194989fe3a7589d1
Steve King <steve@mydev.co>
2020-10-30 07:53:37 +0000

    Add named remote

--- .github/actions/publish/entrypoint.sh
+++ .github/actions/publish/entrypoint.sh
@@ -8,27 +8,28 @@ then
     exit -1
 fi
 
-# Ensure necessary files exist
+# Generate environment variables
+$REMOTE_REPO="https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
+
+# Ensure required files exist
 touch ~/.npmrc
 
 # Setup SSH keys so we can push lerna commits and tags to master branch
 
-#mkdir -p /root/.ssh
-#ssh-keyscan -t rsa github.com > /root/.ssh/known_hosts
-#echo "$GIT_DEPLOY_KEY" > /root/.ssh/id_rsa
-#chmod 400 /root/.ssh/id_rsa
-
 # The script is run as root so we need to allow npm to execute scripts as root.
 echo "unsafe-perm = true" >> ~/.npmrc
 
-# Setup git and pull tags for Lerna
-
+# Configure source control
+git config http.sslVerify false
 git config user.email "$INPUT_EMAIL"
 git config user.name "$INPUT_USERNAME"
 
-git remote set-url origin git@github.com:$GITHUB_REPOSITORY.git
+git remote add kwsites $REMOTE_REPO
+git show-ref
+git fetch kwsites --unshallow --tags
 
-git fetch --unshallow --tags
+git branch --verbose
+git remote --verbose
 
 # Run lerna
-node_modules/.bin/lerna publish $INPUT_EXTRA_ARGUMENTS --registry=$INPUT_REGISTRY --yes $INPUT_BUMP
+# node_modules/.bin/lerna publish $INPUT_EXTRA_ARGUMENTS --git-remote=kwsites --registry=$INPUT_REGISTRY --yes $INPUT_BUMP

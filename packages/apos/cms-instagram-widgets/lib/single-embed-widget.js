d5dfe1e890af078f8e7cccf9acbf704231ab5763
Steve King <steve@mydev.co>
2021-08-22 07:17:59 +0100

    feat: cms-instagram-widgets single post
    As the current official oembed API is being replaced and now requires a full source code review, switch to using the publicly accessible `embed` code as displayed on the website.

--- packages/apos/cms-instagram-widgets/lib/single-embed-widget.js
+++ packages/apos/cms-instagram-widgets/lib/single-embed-widget.js
@@ -1,50 +1,11 @@
-const { promiseResult } = require('@kwsites/promise-result');
-
-module.exports = (self, options) => {
+module.exports = (self) => {
    Object.assign(self, { loadSingleEmbedWidget });
 
-   function getEmbedAuth(req) {
-      const { source, token } = { source: 'inline', ...self.getEmbedConfig(req) };
-      switch (source) {
-         case 'env':
-            return process.env[token];
-         case 'inline':
-            return token;
-         default:
-            throw new Error(`Unknown embed source: "${source}"`);
-      }
-   }
-
-   async function loadSingleEmbedWidget(req, widget) {
+   async function loadSingleEmbedWidget (req, widget) {
       const log = self.log.extend('singleWidget');
       log('singleWidget: loading: %s', widget.url);
 
-      const { threw, result } = await promiseResult(oEmbed(req, widget.url));
-      if (threw) {
-         log('singleWidget: error: %s %o', widget.url, result);
-         widget._embed = null;
-      } else {
-         widget._embed = result;
-         log('singleWidget: complete: %s', widget.url);
-      }
+      widget._instaUrl = widget.url.replace(/\?.+$/, '');
    }
 
-   function oEmbed(req, url) {
-      return new Promise((done, fail) => {
-         self.apos.oembed.query(
-            req,
-            url.replace(/\?.+$/, ''),
-            { neverOpenGraph: true, accessToken: getEmbedAuth(req) },
-            (err, data) => {
-               if (err) {
-                  return fail(err);
-               }
-
-               data.html = data.html.replace(/<script[^>]*>.*<\/script[^>]*>/g, '');
-
-               done(data);
-            }
-         );
-      });
-   }
 };
